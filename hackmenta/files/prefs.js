9b14163937e979f89457364d9d31d6d32611d003
Nicolás Reynolds <fauno@endefensadelsl.org>
2015-08-19 15:38:42 -0300

    anonimizar el user agent closes #16

--- hackmenta/files/prefs.js
+++ hackmenta/files/prefs.js
@@ -7,6 +7,9 @@ user_pref("browser.safebrowsing.enabled", false);
 user_pref("browser.safebrowsing.downloads.enabled", false);
 user_pref("browser.safebrowsing.malware.enabled", false);
 
+// falsificar el user agent
+user_pref("general.useragent.override", "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:37.0) Gecko/20100101 Firefox/37.0");
+
 // Privacidad
 user_pref("datareporting.healthreport.service.enabled", false);
 user_pref("datareporting.healthreport.uploadEnabled", false);
